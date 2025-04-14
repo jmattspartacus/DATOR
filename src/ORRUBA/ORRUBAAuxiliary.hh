7787885814cf94ece2f0853d0101e4f22b322882
Tim Gray <tgray30@utk.edu>
2025-04-14 11:49:39 -0400

    better processing of ORRUBA Scalers

--- src/ORRUBA/ORRUBAAuxiliary.hh
+++ src/ORRUBA/ORRUBAAuxiliary.hh
@@ -5,6 +5,8 @@
 #include <string>
 #include <iostream>
 #include <cmath>
+#include <ctime>
+#include <iomanip>
 
 #include <vector>
 #include <string>
@@ -12,12 +14,34 @@
 #include "Reader/Reader.hh"
 
 namespace Orruba {
+  class Table {
+  public:
+    int ncols;
+    int nrows;
+
+    double *data;
+    std::vector<std::string> names;
+    
+    Table();
+    Table(int nr, int nc);
+    ~Table();
+
+    void Initialize(int nr, int nc);
+    double Get(int row, int col);
+    void Set(int row, int col, double val);
+    void Print();    
+  };
+    
   
   class Auxiliary : public DATOR::Processor {
   public:
     unsigned long long timestamp;
     int nRows;
     char rows[80][81];
+
+    std::string header;
+    time_t time_since_epoch;
+    Table scalers;
     
     Auxiliary() { nRows = 0; }
 
